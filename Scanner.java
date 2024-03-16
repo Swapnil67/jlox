186db5b9481bec1d2b4c01ba03667fbc8eb8fe95
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-03-16 23:36:39 +0530

    Scanner Completed

--- Scanner.java
+++ Scanner.java
@@ -30,7 +30,7 @@ class Scanner {
   }
 
   private void scanToken() {
-    char c - advance();
+    char c = advance();
     switch(c) {
       case '(': addToken(LEFT_PAREN); break;
       case ')': addToken(RIGHT_PAREN); break;
@@ -75,9 +75,58 @@ class Scanner {
         break;
 
       default: 
-        Lox.error(line, 'Unexpected character.');
+        if(isDigit(c)) {
+          number()
+        } 
+        else if(isAlpha()) {
+          identifier()
+        } else {
+          Lox.error(line, 'Unexpected character.');
+        }
         break;
     }
+  }
+
+  private static final Map<String, TokenType? keywords;
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",     AND);
+    keywords.put("class",   CLASS);
+    keywords.put("else",    ELSE);
+    keywords.put("false",   FALSE);
+    keywords.put("for",     FOR);
+    keywords.put("fun",     FUN);
+    keywords.put("if",      IF);
+    keywords.put("nil",     NIL);
+    keywords.put("or",      OR);
+    keywords.put("print",   PRINT);
+    keywords.put("return",  RETURN);
+    keywords.put("super",   SUPER);
+    keywords.put("this",    THIS);
+    keywords.put("true",    TRUE);
+    keywords.put("var",     VAR);
+    keywords.put("while",   WHILE);
+  }
+
+  private void identifier() {
+    while(isAlphaNumeric(peek())) advance();
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if(type == null) type = IDENTIFIER;
+    addToken(type);
+  }
+
+  private void number() {
+    while(isDigit(peek())) advance();
+
+    // * Look for fractional Part
+    if(peek() == '.' && isDigit(peekNext())) {
+      // * Consume the "."
+      advance();
+      while(isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
   }
 
   private void string() {
@@ -107,6 +156,25 @@ class Scanner {
   private char peek() {
     if(isAtEnd) return '\0';
     return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if(current + 1 >= source.length()) return '\0';
+    return source.charAt(current+1);
+  }
+
+  private boolean isAlpha(char c) {
+    return c >= 'a' && c <= 'z' ||
+      c >= 'A' &&  c <= 'Z' ||
+      c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
   }
 
   private boolean isAtEnd() {
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 # jlox
+
+
+# Books
+- https://www.amazon.in/Compilers-2e-Aho/dp/9332518661
