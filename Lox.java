16d73b2c3cda9750bfe0623a93f0354cfc0dfb4b
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-03-16 16:53:31 +0530

    Added Token Class and Scanner class

--- Lox.java
+++ Lox.java
@@ -9,6 +9,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+
+ static bookean hadError = false;
+
   public static void main(String[] args) throws IOException {
     if(args.length > 1) {
       System.out.println("Usage: jlox [script]");
@@ -25,6 +28,8 @@ public class Lox {
   private static void runFile(String path) throws IOException {
     byte[] bytes = Files.readAllBytes(Paths.get(path));
     run(new String(bytes, Charset.defaultCharset()));
+    // * Indicate on error in the exit code
+    if(hadError) System.exit(65);
   }
 
   private static void runPrompt() throws IOException {
@@ -35,7 +40,8 @@ public class Lox {
       System.out.print("> ");
       String line = reader.readLine();
       if(line == null) break;
-      run(line);
+      run(line);  
+      hadError = false;
     }
   }
 
@@ -45,8 +51,17 @@ public class Lox {
 
     // * For now, just print the tokens
     for(Token token: tokens) {
-      System.out.println(token);f
+      System.out.println(token);
     }
+  }
+
+  static void error(int line, String message) {
+    report(line, "", message);
+  }
+
+  private static void report(int line, String where, String message) {
+    System.err.println("[line " + line + "] Error " + where + ": " + message);
+    hadError = true
   }
 
 }
