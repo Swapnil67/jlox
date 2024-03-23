c18629e26de2d8f453b54fd27c2718a990f541e3
swapniladsul017@gmail.com <swapniladsul017@gmail.com>
2024-03-23 22:42:49 +0530

    Completed parser

---
+++ Parser.java
@@ -0,0 +1,192 @@
+package lox;
+
+import java.util.List;
+import static lox.TokenType.*;
+
+class Parser {
+
+  private static class ParseError extends RuntimeException {}
+
+  private final List<Token> tokens;
+  private int current = 0;
+  
+  Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+
+  Expr parse() {
+    try {
+      return expression();      
+    } catch (ParseError e) {
+      return null;
+    }
+  }
+
+  private Expr expression() {
+    return equality();
+  }
+
+  private Expr equality() {
+    Expr expr = comparision();
+    while(match(BANG_EQUAL, EQUAL_EQUAL)) {
+      Token operator = previous();
+      Expr right = comparision();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private Expr comparision() {
+    Expr expr = term();
+    while(match(GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)) {
+      Token operator = previous();
+      Expr right = term();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+
+    return expr;
+  }
+
+  private Expr term() {
+    Expr expr = factor();
+    while(match(MINUS, PLUS)) {
+      Token operator = previous();
+      Expr right = factor();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private Expr factor() {
+    Expr expr = unary();
+    while(match(SLASH, STAR)) {
+      Token operator = previous();
+      Expr right = unary();
+      expr = new Expr.Binary(expr, operator, right);
+    }
+    return expr;
+  }
+
+  private Expr unary() {
+    if(match(BANG, MINUS)) {
+      Token operator = previous();
+      Expr right = unary();
+      return new Expr.Unary(operator, right);
+    }
+    return primary();
+  }
+
+  private Expr primary() {
+    if(match(FALSE)) return new Expr.Literal(false);
+    if(match(TRUE)) return new Expr.Literal(true);
+    if(match(NIL)) return new Expr.Literal(null);
+
+    if(match(NUMBER, STRING)) {
+      return new Expr.Literal(previous().literal);
+    }
+
+    if(match(LEFT_PAREN)) {
+      Expr expr = expression();
+      consume(RIGHT_PAREN, "Expect ')' after expression.");
+      return new Expr.Grouping(expr);
+    }
+
+    throw error(peek(), "Expect expression.");
+  }
+
+  private boolean match(TokenType... types) {
+    for(TokenType type: types) {
+      if(check(type)) {
+        advance();
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+  /**
+   * * Similar to match in that it checks if the next token is of the expected type.
+   * @param type
+   * @param message
+   * @return
+   */
+  private Token consume(TokenType type, String message) {
+    if(check(type)) return advance();
+    throw error(peek(), message);
+  }
+
+  /**
+   * * Returns true if the current token is of the given type.
+   * @param type
+   * @return boolean
+   */
+  private boolean check(TokenType type) {
+    if(isAtEnd()) return false;
+    return peek().type == type;
+  }
+
+  /**
+   * * Consumes the current token and returns it.
+   * @return Token
+   */
+  private Token advance() {
+    if(!isAtEnd()) current++;
+    return previous();
+  }
+
+  /**
+   * * Checks if we've run out of tokens to parse.
+   * @return boolean
+   */
+  private boolean isAtEnd() {
+    return peek().type == EOF;
+  }
+
+  /**
+   * * Returns the current token we have yet to consume
+   * @return Token
+   */
+  private Token peek() {
+    return tokens.get(current);
+  }
+  
+  /**
+   * * Returns most recently consumed token 
+   * @return Token
+   */
+  private Token previous() {
+    return tokens.get(current-1);
+  }
+ 
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
+  }
+
+  /**
+   * * It discards tokens until it thinks it has found a statement boundary
+   */
+  private void synchronize() {
+    advance();
+    while(!isAtEnd()) {
+      if(previous().type == SEMICOLON) return;
+      switch(peek().type) {
+        case CLASS:
+        case FOR:
+        case FUN:
+        case IF:
+        case PRINT:
+        case RETURN:
+        case VAR:
+        case WHILE:
+          return;
+      }
+      advance();
+    }
+  }
+
+  
+}
+
+
